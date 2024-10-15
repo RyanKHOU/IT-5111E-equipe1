7fa982bde73c7b37eab4aaab5a84ad26570e186d
Camman,Anthinea <camman.anthinea@courrier.uqam.ca>
2024-10-15 14:53:58 +0200

    csv generation corrigé ?

--- Database_Generation/csv_generation.py
+++ Database_Generation/csv_generation.py
@@ -3,8 +3,6 @@ import random
 import string
 import datetime
 
-
-
 # Types de places
 types_places = ["moto", "voiture", "camion","handicape"]
 
@@ -15,23 +13,23 @@ def generer_plaque():
     return f"{lettres}-{chiffres}"
 
 # Générer les informations d'un véhicule garé
-def generer_donnees_vehicule(int place):
+def generer_donnees_vehicule(place):
 
     if(place < 20):
         type_place = types_places[3]
     elif(place < 120):
-        type_place = types_places[2]
-    elif(place < 160):
         type_place = types_places[1]
+    elif(place < 160):
+        type_place = types_places[2]
     else :
         type_place = types_places[0]
 
     occupe=random.choice(["oui", "non"])
-    if (occupe = "oui"):
+    if(occupe == "oui"):
         type_vehicule= types_places[random.randint(0, 3)]
         plaque = generer_plaque()
         temps_paye = datetime.timedelta(random.randint(0, 1),random.randint(0, 23),random.randint(0, 59),random.randint(0, 59))
-        prix= random.choice[1.4,3.8,7.3]
+        prix= random.choice([1.4,3.8,7.3])
         horaire_debut=datetime.datetime(2024,10,1,random.randint(0, 23),random.randint(0, 59),random.randint(0, 59),0)
         horaire_fin=horaire_debut + temps_paye
     else:
@@ -40,6 +38,7 @@ def generer_donnees_vehicule(int place):
         prix= None
         horaire_debut= None
         horaire_fin= None
+        plaque = None
 
 
     return {
@@ -47,11 +46,11 @@ def generer_donnees_vehicule(int place):
         "Type de place":type_place,
         "Occupation":occupe,
         "Plaque d'immatriculation":plaque,
-        "Catégorie de véhicule": type_vehicule,
+        "Categorie de véhicule": type_vehicule,
         "Temps paye": temps_paye,
         "Prix paye":prix,
-        "Horaire d'arrivé":horaire_debut,
-        "Horaire de départ prevu":horaire_fin,
+        "Horaire d'arrive":horaire_debut,
+        "Horaire de depart prevu":horaire_fin,
     }
 
 # Générer un CSV avec des véhicules aléatoires
@@ -60,13 +59,13 @@ def generer_csv(nombre_vehicules, fichier_csv):
     colonnes = [
         "Numero de place",
         "Type de place",
-        "Occupartion",
+        "Occupation",
         "Plaque d'immatriculation",
-        "Catégorie de véhicule",
+        "Categorie de véhicule",
         "Temps paye",
         "Prix paye",
-        "Horaire d'arrivé'",
-        "Horaire de départ prevu"
+        "Horaire d'arrive",
+        "Horaire de depart prevu"
     ]
 
     # Générer des données et les écrire dans le fichier CSV
@@ -75,8 +74,8 @@ def generer_csv(nombre_vehicules, fichier_csv):
         writer.writeheader()  # Écrire l'en-tête du CSV
 
         for _ in range(nombre_vehicules):
-            donnees_vehicule = generer_donnees_vehicule()
+            donnees_vehicule = generer_donnees_vehicule(_)
             writer.writerow(donnees_vehicule)
 
 # Exemple d'utilisation pour générer un fichier CSV avec 100 véhicules
-generer_csv(100, "vehicules_gare.csv")
+generer_csv(200, "vehicules_gare.csv")
