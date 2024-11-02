dccbb0dea4107d9ae5d00fbfef027d6b3f4984bb
AnthineaCamman <133351571+AnthineaCamman@users.noreply.github.com>
2024-11-02 15:25:46 +0100

    Add files via upload
    traitement final non testé

--- Traitement.py
+++ Traitement.py
@@ -5,20 +5,41 @@ import datetime
 
 types_places = ["camion", "voiture","moto", "handicape","total"]
 nombre_places = [20,100,40,40,200]
+maintenant =datetime.datetime(2024,10,1,22,00,00,0)
 
 def calcul_places(nom_fichier):
-with open(nom_fichier,mode='r') as file:
+    with open(nom_fichier,mode='r') as file:
     reader = csv.DictReader(file)
     places=(0,0,0,0,0)
     for row in reader:
         if (row['Occupation']=='oui'):
             match row['Numero de place']:
-                case a<20 : places[0]+=1
-                case a>=20 && a< 120:places[1]+=1
-                case a>=120 && a< 160:places[2]+=1
-                case a>=160 :places[3]+=1
+                case a if a<20 : places[0]+=1
+                case a if a>=20 && a< 120:places[1]+=1
+                case a if a>=120 && a< 160:places[2]+=1
+                case a if a>=160 :places[3]+=1
             places[4]+=1
     return places
+
+def calcul_temps(nom_fichier):
+    with open(nom_fichier,mode='r') as file:
+    reader = csv.DictReader(file)
+    liste = []
+    for row in reader:
+        if (row['Occupation']=='oui'):
+            if row['Horaire de depart prevu']< maintenant:
+                liste.append(row['Plaque d'immatriculation'])
+    return liste
+
+def calcul_type(nom_fichier):
+    with open(nom_fichier,mode='r') as file:
+    reader = csv.DictReader(file)
+    liste = []
+    for row in reader:
+        if (row['Occupation']=='oui'):
+            if row['Type de place'] != row['Categorie de véhicule']:
+                liste.append(row['Plaque d'immatriculation'])
+    return liste
 
 def csv_places(nom_fichier):
     # Nom des colonnes
@@ -47,6 +68,9 @@ def csv_infra(nombre_vehicules):
     with open("infraction.csv", mode='w', newline='', encoding='utf-8') as fichier:
         writer = csv.DictWriter(fichier, fieldnames=colonnes)
         writer.writeheader()  # Écrire l'en-tête du CSV
-
-        for _ in range(nombre_vehicules):
-            writer.writerow(donnees_vehicule)
+        donneestps= calcul_temps(nom_fichier)
+        for i in range(len(donneestps)):
+            writer.writerow("Plaque":donneestps[i],"Categorie":"Temps depasse")
+        donneestyp= calcul_type(nom_fichier)
+        for i in range(len(donneestyp)):
+            writer.writerow("Plaque":donneestyp[i],"Categorie":"Type de places")
