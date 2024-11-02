33ab5b9c0a45ee0264e3337857ccd533722b7d23
AnthineaCamman <133351571+AnthineaCamman@users.noreply.github.com>
2024-11-02 16:26:41 +0100

    traitement + n° place

--- Traitement.py
+++ Traitement.py
@@ -12,7 +12,7 @@ def calcul_places(nom_fichier):
         reader = csv.DictReader(file)
     places=(0,0,0,0,0)
     for row in reader:
-        if (row['Occupation']=='oui'):
+        if (row['Occupation']=='non'):
             match row['Numero de place']:
                 case a if a<20 : places[0]+=1
                 case a if a>=20 && a< 120:places[1]+=1
@@ -28,7 +28,7 @@ def calcul_temps(nom_fichier):
     for row in reader:
         if (row['Occupation']=='oui'):
             if row['Horaire de depart prevu']< maintenant:
-                liste.append(row['Plaque d'immatriculation'])
+                liste.append((row["Numero de place"],row["Plaque d'immatriculation"]))
     return liste
 
 def calcul_type(nom_fichier):
@@ -38,7 +38,7 @@ def calcul_type(nom_fichier):
     for row in reader:
         if (row['Occupation']=='oui'):
             if row['Type de place'] != row['Categorie de véhicule']:
-                liste.append(row['Plaque d'immatriculation'])
+                liste.append((row["Numero de place"],row["Plaque d'immatriculation"]))
     return liste
 
 def csv_places(nom_fichier):
@@ -61,6 +61,7 @@ def csv_places(nom_fichier):
 def csv_infra(nom_fichier):
     # Nom des colonnes
     colonnes = [
+        "Place"
         "Plaque",
         "Categorie"
     ]
@@ -73,9 +74,9 @@ def csv_infra(nom_fichier):
             writer.writerow("Plaque":donneestps[i],"Categorie":"Temps depasse")
         donneestyp= calcul_type(nom_fichier)
         for i in range(len(donneestyp)):
-            writer.writerow("Plaque":donneestyp[i],"Categorie":"Type de places")
+            writer.writerow("Place":donneestyp[i][0],"Plaque":donneestyp[i][1],"Categorie":"Type de places")
 
 
 def main(nom_fichier):
-    csv_places(nom_fichier)
+    print(calcul_places(nom_fichier))
     csv_infra(nom_fichier)
